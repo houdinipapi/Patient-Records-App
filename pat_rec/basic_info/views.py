44583fc3bd7200d21dd17389fd3c96e7d783182c
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-05 18:00:39 +0300

    Update .hintrc and base.html, add login functionality

--- pat_rec/basic_info/views.py
+++ pat_rec/basic_info/views.py
@@ -1,6 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def index(request):
-    return render(request, "core/index.html", {})
+    # Check to see if logging in
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if user is valid
+        user = authenticate(request, username=username, password=password)
+
+        # If user is found, log them in
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"Welcome, {username}!")
+            return redirect("index")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect("index")
+    else:
+        return render(request, 'core/index.html', {})
+
+
+def logout_user(request):
+    pass
