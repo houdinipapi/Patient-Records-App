3d8495732bbc5443bc0c5c510cd1c96c51a32fde
Houdini Papi <ochiengcliff.co@gmail.com>
2024-02-06 22:25:59 +0300

    Add created_by field to Patient model

--- pat_rec/basic_info/models.py
+++ pat_rec/basic_info/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Patient(models.Model):
@@ -12,6 +13,7 @@ class Patient(models.Model):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=50)
     zipcode = models.CharField(max_length=20)
+    created_by = models.ForeignKey(User, related_name= 'patients', on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
